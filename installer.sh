35345d1e58cb354fc532740502a7e19469a1639a
Adolfo De Unánue <nanounanue@gmail.com>
2017-03-13 14:24:59 -0600

    Fixes #2

--- installer.sh
+++ installer.sh
@@ -1,15 +1,35 @@
-#!/bin/sh
+while getopts ":p:u:" opt; do
+    case $opt in
+        p) PROJECT_NAME="$OPTARG"
+           ;;
+        u) USERNAME="$OPTARG"
+           ;;
+        \?) echo "Opción inválida -$OPTARG" >&2
+            ;;
+    esac
+done
 
-PROJECT_NAME:=$0
 
-git init $(PROJECT_NAME)
+git init ${PROJECT_NAME}
 
-cd $(PROJECT_NAME)
+cd ${PROJECT_NAME}
 
-git remote add "template" https://github.com/nanounanue/pipeline-template.git
+git remote add "template" https://github.com/${USERNAME}/pipeline-template.git
 
 git pull template master
+
+echo ${PROJECT_NAME} > .project-name
+
+hub create ${USERNAME}/${PROJECT_NAME}
+
+git push origin master
 
 git flow init -d
 
 git checkout develop
+
+make help 
+
+
+
+
